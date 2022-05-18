d7d2219cb6df159c8b696a4dce61dbcd144fe2e0
Charles Averill <charlesaverill20@gmail.com>
2022-05-17 22:24:59 -0500

    update CMakeLists.txt to support install and debug builds build_run.sh now makes a debug build by default, and installs with the 'install' argument

--- build_run.sh
+++ build_run.sh
@@ -1,14 +1,36 @@
-echo "---CLANG-FORMAT----"
-clang-format -i $(find src -name "*.c") $(find include -name "*.h")
-echo "------CMAKE--------"
-if cmake -S . -B build ; then
-    echo "-------MAKE--------"
-    if cmake --build build -j$(nproc) ; then
-        echo "-----EXECUTING-----"
-        bin/aftn $*
+if [ $# -gt 0 ] ; then
+    if [ $1 = "install" ] ; then
+      echo "---CLANG-FORMAT----"
+      clang-format -i $(find src -name "*.c") $(find include -name "*.h")
+      echo "------CMAKE--------"
+      if sudo cmake -S . -B build -DDebugBuild=OFF ; then
+          echo "-------MAKE--------"
+          if sudo cmake --build build --target install ; then
+              echo "-----EXECUTING-----"
+              aftn $*
+          else
+              echo "-----MAKE FAILURE-----"
+          fi
+      else
+          echo "-----CMAKE FAILURE-----"
+      fi
     else
-        echo "-----MAKE FAILURE-----"
+      echo "Argument \"$1\" not recognized"
+      exit 1
     fi
 else
-    echo "-----CMAKE FAILURE-----"
+    echo "---CLANG-FORMAT----"
+    clang-format -i $(find src -name "*.c") $(find include -name "*.h")
+    echo "------CMAKE--------"
+    if cmake -S . -B build -DDebugBuild=ON ; then
+        echo "-------MAKE--------"
+        if cmake --build build -j$(nproc) ; then
+            echo "-----EXECUTING-----"
+            bin/aftn $*
+        else
+            echo "-----MAKE FAILURE-----"
+        fi
+    else
+        echo "-----CMAKE FAILURE-----"
+    fi
 fi
